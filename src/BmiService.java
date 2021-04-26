54642ceba8c374678f68f80d319ee9691ec1f657
Alina <77508351+Alina1103master@users.noreply.github.com>
2021-04-26 22:17:16 +0300

    initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,11 +1,7 @@
+
 public class BmiService {
-    public int calculate(int height, double weight) {
-        height = 1_75;
-        weight = 74;
-        double bmi =  weight / height ;
-        return (int) (weight / height);
-
+    public double calculate(double weight, double height) {
+        double bmi =weight/height;
+        return bmi;
     }
-
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService ();
-        double height,weight;
-        height = 1_75;
-        weight = 74;
-
-        int bmi = service.calculate((int) height,weight);
+        BmiService service = new BmiService();
+        double weight=50.0;
+        double height=1.75;
+        double bmi = service.calculate(weight, height);
         System.out.println(bmi);
     }
 }
